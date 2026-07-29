87c784d225fa0d62903f0c5ed154cee22e91b0eb
damon <zhaorong@outlook.com>
2026-07-30 00:01:20 +0800

    Refactor IMessageConvention interface and add IMessageConventionBuilder for message type classification

--- Source/Euonia.Bus.Abstract/Conventions/IMessageConvention.cs
+++ Source/Euonia.Bus.Abstract/Conventions/IMessageConvention.cs
@@ -13,30 +13,30 @@ public interface IMessageConvention
 	/// <summary>
 	/// Determine if a type is a unicast type.
 	/// </summary>
-	/// <param name="messageType">The type to check.</param>.
-	/// <returns>true if <paramref name="messageType"/> represents a unicast message.</returns>
+	/// <param name="channel">The type to check.</param>.
+	/// <returns>true if <paramref name="channel"/> represents a unicast message.</returns>
 	/// <remarks>
 	///	The unicast message is delivered to a single recipient, typically using a queue-based mechanism.
 	/// </remarks>
-	bool IsUnicastType(Type messageType);
+	bool IsUnicastType(string channel);
 
 	/// <summary>
 	/// Determine if a type is a multicast type.
 	/// </summary>
-	/// <param name="messageType">The type to check.</param>.
-	/// <returns>true if <paramref name="messageType"/> represents a topic message.</returns>
+	/// <param name="channel">The type to check.</param>.
+	/// <returns>true if <paramref name="channel"/> represents a topic message.</returns>
 	/// <remarks>
 	///	The multicast message is delivered to multiple recipients, typically using a topic-based mechanism.
 	/// </remarks>
-	bool IsMulticastType(Type messageType);
+	bool IsMulticastType(string channel);
 
 	/// <summary>
 	/// Determine if a type is a request type.
 	/// </summary>
-	/// <param name="messageType">The type to check.</param>
-	/// <returns>true if <paramref name="messageType"/> represents a request message.</returns>
+	/// <param name="channel">The type to check.</param>
+	/// <returns>true if <paramref name="channel"/> represents a request message.</returns>
 	/// <remarks>
 	/// The request message is sent to a single recipient, expecting a response.
 	/// </remarks>
-	bool IsRequestType(Type messageType);
+	bool IsRequestType(string channel);
 }
