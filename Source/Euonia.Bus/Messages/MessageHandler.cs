dc4811fc172ed7fbb0ed19ccf76b8b378c66a6b4
damon <zhaorong@outlook.com>
2026-07-30 17:20:03 +0800

    Refactor MessageHandler delegates to use IMessageContext for improved message handling

--- Source/Euonia.Bus/Messages/MessageHandler.cs
+++ Source/Euonia.Bus/Messages/MessageHandler.cs
@@ -4,10 +4,10 @@
 /// 
 /// </summary>
 /// <typeparam name="TMessage"></typeparam>
-public delegate Task MessageHandler<in TMessage>(TMessage message, MessageContext context, CancellationToken cancellationToken = default)
+public delegate Task MessageHandler<in TMessage>(TMessage message, IMessageContext context, CancellationToken cancellationToken = default)
 	where TMessage : class;
 
 /// <summary>
 /// 
 /// </summary>
-public delegate Task<object> MessageHandler(object message, MessageContext context, CancellationToken cancellationToken = default);
+public delegate Task<object> MessageHandler(object message, IMessageContext context, CancellationToken cancellationToken = default);
