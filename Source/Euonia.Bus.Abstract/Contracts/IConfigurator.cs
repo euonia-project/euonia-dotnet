df8d117bcc703841664a7922076959917ec2e3d1
damon <zhaorong@outlook.com>
2026-07-30 17:19:43 +0800

    Rename IBusConfigurator to IConfigurator for improved clarity and update method signatures and documentation

---
+++ Source/Euonia.Bus.Abstract/Contracts/IConfigurator.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+
+namespace Nerosoft.Euonia.Bus;
+
+/// <summary>
+/// <see cref="IConfigurator"/>是配置消息总线的主接口。
+/// 提供了配置消息约定、传输策略和处理器注册的方法。此接口的实现将在总线启动之前用于设置总线。
+/// </summary>
+public interface IConfigurator
+{
+	/// <summary>
+	/// 获取默认传输策略名称，可用于消息路由和分类。
+	/// </summary>
+	/// <returns>默认传输策略名称。</returns>
+	string DefaultTransport { get; }
+	
+	/// <summary>
+	/// 获取已配置的 <see cref="IMessageConventionBuilder"/>。
+	/// </summary>
+	/// <returns>已配置的 <see cref="IMessageConventionBuilder"/> 实例。</returns>
+	IMessageConventionBuilder ConventionBuilder { get; }
+	
+	/// <summary>
+	/// 获取已配置的传输策略构建器映射。
+	/// </summary>
+	/// <returns>已配置的传输策略构建器映射。</returns>
+	IDictionary<string, TransportStrategyBuilder> StrategyBuilders { get; }
+
+	/// <summary>
+	/// 获取消息约定，可用于消息格式化和验证。
+	/// </summary>
+	/// <returns>已配置的 <see cref="IMessageConvention"/> 实例。</returns>
+	IMessageConvention Convention => ConventionBuilder.Convention;
+
+	/// <summary>
+	/// 获取传输策略名称列表，可用于消息路由和分类。
+	/// </summary>
+	/// <returns>传输策略名称列表。</returns>
+	List<string> StrategyAssignedTypes => [.. StrategyBuilders.Keys];
+	
+	/// <summary>
+	/// 获取指定传输名称对应的传输策略，可用于消息路由和分类。
+	/// </summary>
+	/// <param name="transport">传输名称。</param>
+	/// <returns>对应的 <see cref="TransportStrategy"/> 实例。</returns>
+	TransportStrategy Strategy(string transport)
+	{
+		return StrategyBuilders.TryGetValue(transport, out var builder) ? builder.Strategy : null;
+	}
+}
