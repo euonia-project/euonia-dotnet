b7d6813531833defca35b2ce1d4779be6e83177b
damon <zhaorong@outlook.com>
2026-09-02 22:27:54 +0800

    Add Constants class for ActiveMq and reference to Euonia.Modularity project

---
+++ Source/Euonia.Bus.ActiveMq/Constants.cs
@@ -0,0 +1,12 @@
+namespace Nerosoft.Euonia.Bus.ActiveMq;
+
+/// <summary>
+/// 常量定义。
+/// </summary>
+internal class Constants
+{
+	/// <summary>
+	/// 获取默认的传输器名称。
+	/// </summary>
+	public const string DefaultTransportName = "ActiveMq";
+}
