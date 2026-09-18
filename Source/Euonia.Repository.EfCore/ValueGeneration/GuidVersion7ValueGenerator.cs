4643d5e89c68ac8dd79cd066d47428171aa6310c
damon <zhaorong@outlook.com>
2026-09-18 22:33:02 +0800

    Add GuidVersion7ValueGenerator for EF Core support

---
+++ Source/Euonia.Repository.EfCore/ValueGeneration/GuidVersion7ValueGenerator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+
+namespace Nerosoft.Euonia.Repository.EfCore;
+
+/// <summary>
+/// 
+/// </summary>
+public class GuidVersion7ValueGenerator : ValueGenerator<Guid>
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public override bool GeneratesTemporaryValues { get; } = false;
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="entry"></param>
+	/// <returns></returns>
+	public override Guid Next(EntityEntry entry)
+	{
+		return Guid.CreateVersion7();
+	}
+}
